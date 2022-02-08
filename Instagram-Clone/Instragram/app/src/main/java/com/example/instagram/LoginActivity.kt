a00caa4aa06974b50cc82801867000d4229f7f6e
dkswnkk <jjoy9810@gmail.com>
2022-02-08 17:32:13 +0900

    feat: 구글 로그인 구현

--- Instagram-Clone/Instragram/app/src/main/java/com/example/instagram/LoginActivity.kt
+++ Instagram-Clone/Instragram/app/src/main/java/com/example/instagram/LoginActivity.kt
@@ -1,19 +1,27 @@
 package com.example.instagram
 
-import android.app.Application
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.instagram.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
     var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         FirebaseApp.initializeApp(this)
@@ -22,10 +30,45 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
         binding.emailLoginButton.setOnClickListener {
-           signinAndsignup()
+            signinAndsignup()
         }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.firebase_web_client_id_key)).requestEmail().build()
 
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
+        binding.googleSignInButton.setOnClickListener {
+            Log.d("test","구글 로그인 버튼 누름")
+            googleLogin()
+        }
+    }
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
+            Log.d("test","${result?.status}")
+            if (result?.isSuccess!!) {
+                var account = result.signInAccount
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)?.addOnCompleteListener { it ->
+            if (it.isSuccessful) {
+                moveMainPage(it.result?.user)
+            } else {
+                Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     fun signinAndsignup() {
@@ -38,7 +81,7 @@ class LoginActivity : AppCompatActivity() {
                 moveMainPage(it.result?.user)
             } else if (it.exception?.message.isNullOrEmpty()) {
                 // 에러
-                Toast.makeText(this,it.exception?.message,Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
             } else {
                 //
                 signInEmail()
@@ -53,15 +96,20 @@ class LoginActivity : AppCompatActivity() {
         )?.addOnCompleteListener { it ->
             if (it.isSuccessful) {
 //                 아이디와 패스워드가 일치하지 않을때
-            } else{
+            } else {
 //                 아이디와 패스워드가 틀렸을때
             }
         }
     }
 
-    fun moveMainPage(user: FirebaseUser?){
-        if(user !=null){
-            startActivity(Intent(this,MainActivity::class.java))
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            )
         }
     }
 }

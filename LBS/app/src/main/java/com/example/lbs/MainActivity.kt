d78c29646b9dad64f757e6852365d2dad0997846
dkswnkk <jjoy9810@gmail.com>
2022-02-12 20:41:06 +0900

    feat: splash 이미지 추가

--- LBS/app/src/main/java/com/example/lbs/MainActivity.kt
+++ LBS/app/src/main/java/com/example/lbs/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.lbs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import com.example.lbs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        SystemClock.sleep(1000)
+        setTheme(R.style.Theme_LBS)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
